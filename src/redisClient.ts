e8f958ebb1a16960867db67936fda2c354a4eb3e
Aditya Raj Kumawat <arkumawat78@gmail.com>
2021-09-11 21:44:08 +0530

    dockerised with redis

--- src/redisClient.ts
+++ src/redisClient.ts
@@ -1,6 +1,10 @@
 import Redis, { RedisOptions } from 'ioredis'
+import { __prod__ } from './constants'
 
 // add production configs later
-const config: RedisOptions = {}
+const config: RedisOptions = {
+    host: 'redisdb',
+    port: 6379,
+}
 
 export const redis = new Redis(config)
